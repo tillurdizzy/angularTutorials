ef503d2d3c5219e016e9a3595e6a79eeadafa6d3
James <tillurdizzy@live.com>
2017-07-09 08:14:58 -0500

    july 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 // Paths relative to src 
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { SearchComponent } from './search/search.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
